77ae1e22986253bc4b007d6c69b8bf402a5dd4b5
regi <cabael.regine@gmail.com>
2022-04-08 15:00:03 +0200

    nuovoProdotto

---
+++ Csharp Shop/Prodotto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Shop
+{
+    internal class Prodotto
+    {
+        private int codice;
+        public string nome;
+        public string descrizione;
+        public double prezzo;
+        public double iva;
+
+        public int codiceNuovoProdotto
+        {
+            set
+            {
+                Random r = new Random();
+                codice = r.Next();
+                Console.WriteLine("Codice: " + codice);
+            }
+            get
+            {
+                return codice;
+            }
+        }
+
+        public void stampaPrezzoBase()
+        { 
+            Console.WriteLine("Il prezzo di base è: " + prezzo + " euro");
+        }
+
+        public void calcoloIva()
+        {
+            double risultato = Math.Round(prezzo * 0.22 + prezzo, 2);
+            Console.WriteLine("Il prezzo con Iva è: " + risultato + " euro");
+        }
+
+        
+
+    }
+}
--- Csharp Shop/Program.cs
+++ Csharp Shop/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using Csharp_Shop;
+
+Prodotto nuovoProdotto = new Prodotto();
+
+nuovoProdotto.prezzo = 10;
+nuovoProdotto.nome = "Panino al prosciutto";
+nuovoProdotto.stampaPrezzoBase();
+
+nuovoProdotto.calcoloIva();
+Console.WriteLine(nuovoProdotto.codiceNuovoProdotto);
+//nuovoProdotto.stampaNomeEsteso();
